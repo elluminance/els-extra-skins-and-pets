1a6fa89a454e1357e255d0e6578b5eee38673fa4
EL2020 <el2020202020@gmail.com>
2021-08-31 09:54:41 -0400

    increased item api version requirement

--- prestart.js
+++ prestart.js
@@ -20,22 +20,10 @@ function getSpecialColorHSV(hueAtMax, hueAtMin, rotPercent) {
         return color
     }
 
-    function dec2hex(num){
-        if (num >= 0 && num <= 9) return num
-        switch(num){
-            case 10: return "A"
-            case 11: return "B"
-            case 12: return "C"
-            case 13: return "D"
-            case 14: return "E"
-            case 15: return "F"
-        }
-    }
-
     let red = Math.floor(hueToRGBValue(newHue + 120))
     let green = Math.floor(hueToRGBValue(newHue))
     let blue = Math.floor(hueToRGBValue(newHue - 120))
-    return `#${dec2hex(red)}${dec2hex(green)}${dec2hex(blue)}`;
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
 
 ig.EFFECT_ENTRY.COPY_SPRITE_SPECIAL_COLOR_EL = ig.EffectStepBase.extend({
